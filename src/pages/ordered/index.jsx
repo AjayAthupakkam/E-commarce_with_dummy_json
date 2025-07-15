cdec26e96b57d8848722b4da3b573e373c3a700e
AjayAthupakkam <user@example.com>
2025-07-15 16:23:42 +0530

    added shopping button

--- src/pages/ordered/index.jsx
+++ src/pages/ordered/index.jsx
@@ -1,13 +1,18 @@
 import orderGif from '../../assets/orderGif.gif'
+import {useNavigate} from "react-router-dom";
+
 
 
 
 function Ordered(){
-    return <div className="flex justify-center items-center h-screen ">
+    const navigate  = useNavigate()
+    return <div className="flex flex-col justify-center items-center h-screen ">
         <img 
         src={orderGif}
         alt="Loading..."
          />
+         <button onClick={()=>navigate("/products")} className="  mt-5 min-w-[200px] px-4 py-3 border border=[#333]  bg-black text-sm text-white font-semibold
+                 rounded cursor-pointer">Continue Shopping</button>
     </div>
 }
 
